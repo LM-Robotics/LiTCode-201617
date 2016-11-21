fc431d8da062392339b982c3cc4156e444211ad5
AbdullahNaveedAbdullahNaveed <weeheejee@gmail.com>
2016-11-20 18:31:55 -0600

    Revert "public class"
    This reverts commit 9f815e0f4fd22409bc332721d5471797a12a63f3.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Actions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Actions.java
@@ -1,26 +1,14 @@
 package org.firstinspires.ftc.teamcode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import org.firstinspires.ftc.teamcode.Devices.DriveSystem;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import org.firstinspires.ftc.teamcode.Devices.DriveSystem;
-import org.firstinspires.ftc.teamcode.Devices.FlyWheelMechanic;
-import org.firstinspires.ftc.teamcode.Devices.SweeperMechanic;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.Range;
+
 
 
 /**
  * Created by adsweiger on 11/12/2016.
  */
-@Autonomous(name = "name = Pompeii: Autonomous Tank", group = "Sensor")
+
 public class Actions {
-    public DriveSystem drive;
-
-
     public void inti(){
-        drive = new DriveSystem(hardwareMap);
+
     }
     public void loop(){
 
