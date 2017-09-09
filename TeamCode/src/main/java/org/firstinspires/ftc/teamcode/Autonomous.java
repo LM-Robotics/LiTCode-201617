e55b7e582c631eeda0fc2a8b28f3e72c8edfca2b
Lost in Time <lostintime4324@gmail.com>
2017-09-09 16:38:31 -0500

    TeleOp and autonomous templates MotorController is to make controlling motors easier and less redundant MotorNames is to keep all motor names used by the phones in one place. Use this to add or change motor names Navigation is used for moving the robot during autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -1,37 +1,16 @@
 package org.firstinspires.ftc.teamcode;
-import org.firstinspires.ftc.teamcode.Devices.TrapDoorMechanic;
-import org.firstinspires.ftc.teamcode.Navigation;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import org.firstinspires.ftc.teamcode.Devices.FlyWheelMechanic;
-/**
- * Created by adsweiger on 11/12/2016.
- */
+
 @com.qualcomm.robotcore.eventloop.opmode.Autonomous(name="Pompeii: Autonomous", group="Pompeii")
-
-
 public class Autonomous extends LinearOpMode {
 
-    private FlyWheelMechanic flywheel;
-    private TrapDoorMechanic trapDoor;
     private Navigation nav;
 
-
     @Override
-    public void runOpMode() throws InterruptedException
-    {
-        flywheel = new FlyWheelMechanic(hardwareMap);
-        trapDoor = new TrapDoorMechanic(hardwareMap);
+    public void runOpMode() throws InterruptedException {
         nav = new Navigation(hardwareMap);
-        trapDoor.setPosition(.5);
-
         waitForStart();
-        Thread.sleep(15000);
-        flywheel.setPower(-0.8);
-        nav.driveForward(.5,500);
-        Thread.sleep(2000);
-        trapDoor.setPosition(0);
-        Thread.sleep(500);
-        flywheel.setPower(0);
-        nav.driveForward(.5,1500);
+
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -1,155 +1,27 @@
 package org.firstinspires.ftc.teamcode;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import org.firstinspires.ftc.teamcode.Devices.DriveSystem;
-import org.firstinspires.ftc.teamcode.Devices.FlyWheelMechanic;
-import org.firstinspires.ftc.teamcode.Devices.SweeperMechanic;
-import org.firstinspires.ftc.teamcode.Devices.TrapDoorMechanic;
-import org.firstinspires.ftc.teamcode.Devices.BeaconPresserServo.beaconServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
-
-
-
-/**
- * Created by abnaveed on 10/13/2016.
- */
 
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="Pompeii: Teleop Tank", group="Pompeii")
-public class TeleOp extends OpMode
-{
-    private DriveSystem drive;
-    private FlyWheelMechanic flywheel;
-    private SweeperMechanic sweeper;
-    private TrapDoorMechanic trapdoor;
-    private beaconServo beacon;
-
-    private boolean beaconPosition = false;
-    private boolean xPressedOff = false;
-    private boolean bumperPressedOff = false;
-    private boolean flyWheelOn = false;
-    private boolean buttonAPressedOff = false;
-    private boolean trapDoorDown =  false;
+public class TeleOp extends OpMode {
     // TeleOp
-    @Override
-    public void init()
-    {
-        flywheel = new FlyWheelMechanic(hardwareMap);
-        sweeper = new SweeperMechanic(hardwareMap);
-        trapdoor = new TrapDoorMechanic(hardwareMap);
-        drive = new DriveSystem(hardwareMap);
-        beacon = new beaconServo(hardwareMap);
-        flywheel.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        flywheel.setTargetPosition(200);
+    private MotorController leftMotors;
+    private MotorController rightMotors;
 
+    @Override
+    public void init() {
+        leftMotors = new MotorController(hardwareMap);
+        leftMotors.add(MotorNames.BACK_LEFT_DRIVE.get());
+        leftMotors.add(MotorNames.FRONT_LEFT_DRIVE.get());
+        rightMotors = new MotorController(hardwareMap);
+        rightMotors.add(MotorNames.BACK_RIGHT_DRIVE.get());
+        rightMotors.add(MotorNames.FRONT_RIGHT_DRIVE.get());
     }
+
     @Override
-    public void loop()
-    {
-        double leftStick = gamepad1.left_stick_y;
+    public void loop() {
+        leftMotors.setPower(gamepad1.left_stick_y);
+        rightMotors.setPower(gamepad1.right_stick_y);
 
-        double rightStick = gamepad1.right_stick_y;
-
-        int encoderPosition = flywheel.getCurrentPosition();
-
-        telemetry.addData("Encoder Position", encoderPosition);
-
-        flywheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        if(leftStick > 0.05 || leftStick < -0.05)
-        {
-           drive.setLeft(leftStick);
-        }
-        else
-        {
-            drive.setLeft(0);
-        }
-
-        if (rightStick > 0.05 || rightStick< -0.05)
-        {
-            drive.setRight(rightStick);
-        }
-        else
-        {
-            drive.setRight(0);
-        }
-
-        if (!(gamepad2.a))
-        {
-            buttonAPressedOff = true;
-        }
-
-        if (trapDoorDown && gamepad2.a && buttonAPressedOff)
-        {
-            trapDoorDown = false;
-            buttonAPressedOff = false;
-        }
-        else if (!(trapDoorDown) && gamepad2.a && buttonAPressedOff)
-        {
-            trapDoorDown = true;
-            buttonAPressedOff = false;
-        }
-
-        if (trapDoorDown)
-        {
-            trapdoor.setPosition(0);
-        }
-        else
-        {
-            trapdoor.setPosition(0.5);
-        }
-
-        if (!(gamepad2.right_bumper))
-        {
-            bumperPressedOff = true;
-        }
-
-        if (flyWheelOn && gamepad2.right_bumper && bumperPressedOff)
-        {
-            flyWheelOn = false;
-            bumperPressedOff = false;
-        }
-        else if (!(flyWheelOn) && gamepad2.right_bumper && bumperPressedOff)
-        {
-            flyWheelOn = true;
-            bumperPressedOff = false;
-        }
-
-        if (flyWheelOn)
-        {
-
-            flywheel.setPower(-.8);
-        }
-        else
-        {
-            flywheel.setPower(0);
-        }
-
-        if (gamepad2.dpad_down)
-        {
-            sweeper.setPower(-1);
-        } else if (gamepad2.dpad_up)
-        {
-            sweeper.setPower(1);
-        }
-        else
-        {
-            sweeper.setPower(0);
-        }
-
-        if (!(gamepad2.x))
-        {
-            xPressedOff = true;
-        }
-        if (gamepad2.x && xPressedOff && beaconPosition)
-        {
-            beaconPosition = false;
-            xPressedOff = false;
-            beacon.setBeaconPosition(0);
-        }
-        else if (gamepad2.x && xPressedOff&& !(beaconPosition))
-        {
-            beaconPosition = true;
-            xPressedOff = false;
-            beacon.setBeaconPosition(.4);
-        }
     }
 }
